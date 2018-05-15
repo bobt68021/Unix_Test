9e2514754382b17ac878e236f5855f383356723f
bobt68021 <tishma.1@osu.edu>
2018-05-15 19:12:44 -0400

    FileHandler2.0

--- test.rb
+++ test.rb
@@ -1,23 +1,32 @@
+require 'fileutils'
+require "watir-webdriver"
+# file = "";
+# File.open("C:\\Devl\\Projects\\from\\test4.txt", "r") do |f|
+#   f.each_line do |line|
+#     file = file + line
+#   end
+# end
+#
+# p file
+#
+#
+# out_file = File.new("C:\\Devl\\Projects\\to\\test4.txt", "w")
+#   #...
+# out_file.puts(file)
+#   #...
+# out_file.close
 
-file = "";
-File.open("./test2.txt", "r") do |f|
-  f.each_line do |line|
-    file = file + line
-  end
-end
-
-p file
-
-
-out_file = File.new("out.txt", "w")
-  #...
-out_file.puts(file)
-  #...
-out_file.close
 
 
 
+ff = Watir::Browser.new(:chrome)
+ff.goto('www.google.com')
+ff.text_field(:id => 'q').select
+ff.send_keys 'hello'
+ff.send_keys :enter
+sleep 3
 
+FileUtils.cp('.\in.txt', '.\out.txt')
 
 
 
@@ -34,5 +43,5 @@ out_file.close
 # my_dir.each do |filename|
 #   name = File.basename('filename', '.doc')[0,4]
 #   dest_folder = "C:\\Devl\\Projects#{name}"
-#   FileUtils.cp('test2.txt', 'test3.txt')
+#   FileUtils.cp('in.txt', 'test3.txt')
 # end
