b27bcace5dcec7cc3a9d441e3b93e77a42765c6c
bobt68021 <tishma.1@osu.edu>
2018-05-15 18:39:57 -0400

    save

--- test.rb
+++ test.rb
@@ -1,1 +1,27 @@
 p "hey"
+
+  # FileUtils.cp('C:\Devl\Projects\from\test.txt', 'C:\Devl\Projects\to\test.txt')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# my_dir = Dir[".\\"]
+# my_dir.each do |filename|
+#   name = File.basename('filename', '.doc')[0,4]
+#   dest_folder = "C:\\Devl\\Projects#{name}"
+  FileUtils.cp('test2.txt', 'test3.txt')
+# end
